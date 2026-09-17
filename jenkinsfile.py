d51cd335d3b518e81717af451164c29606920338
rishikaa-3639 <duntyrishikaa007@gmail.com>
2026-09-17 14:58:06 +0530

    Add Jenkins pipeline for report generation and archiving
    This Jenkins pipeline script includes stages for checking out the code, generating a report by running a Python script, and archiving the report artifact.

---
+++ jenkinsfile.py
@@ -0,0 +1,23 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'main', url: 'https://github.com/<student-username>/<repo-name>.git'
+            }
+        }
+
+        stage('Generate Report') {
+            steps {
+                bat 'python app.py'
+            }
+        }
+
+        stage('Archive Report') {
+            steps {
+                archiveArtifacts artifacts: 'report.txt', fingerprint: true
+            }
+        }
+    }
+}
